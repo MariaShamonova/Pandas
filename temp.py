fafcaeb14d72242095621aa57639580ab34ef639
mshamonova <mshamonova@saber3d.ru>
2021-11-01 23:19:58 +0300

    1: output number of observations, 2: output title of columns, 3: most common value, 4: made plotted frequencies

--- temp.py
+++ temp.py
@@ -4,7 +4,23 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    data = pd.read_csv('https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv', sep='\t')
+    pd.options.display.max_columns = None
+    # pd.options.display.max_colwidth = None
+    temp = data['choice_description'].head(20)
+    
+    number_observerations = len(data.index)
+    all_columns = data.columns
+    most_common_values = data['item_name'].mode().values
 
-    print('Create project 1')
+  
+    print("Number of observations: ",number_observerations)
+    print('Columns: ', ', '.join(list(data.columns)))
+    print("Most common values: ",  ', '.join(most_common_values))
+    
+    data_frequency = data['item_name'].value_counts()
+    data_frequency.plot(kind='bar', figsize=(95,30), fontsize=100)  
