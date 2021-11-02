332fa1de1b30f0e8e0708c7d544005f5e5fe086d
mshamonova <mshamonova@saber3d.ru>
2021-11-02 08:36:03 +0300

    change type of column 'item_price' from object to float64

--- temp.py
+++ temp.py
@@ -5,7 +5,6 @@ Spyder Editor
 This is a temporary script file.
 """
 import pandas as pd
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     data = pd.read_csv('https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv', sep='\t')
@@ -24,3 +23,10 @@ if __name__ == "__main__":
     
     data_frequency = data['item_name'].value_counts()
     data_frequency.plot(kind='bar', figsize=(95,30), fontsize=100)  
+    
+    col = 'item_price'
+    old_type_column = data[col].dtypes
+    data[col] = data[col].apply(lambda x: float(x.replace('$', '').replace(' ', '')))
+    new_type_column = data[col].dtypes
+    print("Change type of column 'item_price' from ", old_type_column, " to ", new_type_column)
+    
